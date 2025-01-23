272899cce44aa1eed930b8f8daf1436bab66c543
Sakura <sakurathetrans@gmail.com>
2025-01-23 12:48:11 -0500

    Actually need to update the main.java smh

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String city = "Columbus";
+        int zip = 43215;
+        int[] dailyTemps = {23,54,62,24,10};
+        double sum = 0;
+        for(int i=0; i<dailyTemps.length; i++){
+            sum += dailyTemps[i];
+        }
+        double avg = sum / dailyTemps.length;
+        System.out.println("City : " + city + " Zip Code : " + zip + " Average High Temperature : " + avg);
     }
 }
